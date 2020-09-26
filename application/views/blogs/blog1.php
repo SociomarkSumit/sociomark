684d4757d8f3b29d135c65bcc14eb65518aaf89f
SociomarkSumit <71916852+SociomarkSumit@users.noreply.github.com>
2020-09-26 15:49:06 +0530

    blog post

--- application/views/blogs/blog1.php
+++ application/views/blogs/blog1.php
@@ -19,21 +19,269 @@
 	</div>
 </div>
 <?php include(APPPATH.'views\layout\header.php');?>
-<main class="main-root">
+	<main class="main-root">
 	<div id="dsn-scrollbar">
 		<header>
-			<div class="container header-hero">
-				<div class="row">
-					<div class="col-lg-6">
-						<div class="contenet-hero">
-							<h5 class="color1 " >NEWS & IDEAS</h5>
-							<h1 class="color2" >Our Blog</h1>
+			<div class="header-single-post" data-dsn-header="project">
+				<div class="post-parallax-wrapper" data-overlay="3">
+					<img id="dsn-hero-parallax-img" class="w-100 has-top-bottom" src="assets/img/blog/4.jpg" alt=""
+						 data-dsn-header="blog" data-dsn-ajax="img">
+				</div>
+				<div class="container">
+					<div class="inner-box m-section">
+						<div class="post-info">
+							<a href="#" class="blog-post-date dsn-link">2 september, 2019</a>
+							<div class="blog-post-cat dsn-link">
+								<a href="#">Uncategorized</a>
+							</div>
 						</div>
+						<h3 class="title-box mt-10">How to Write Great Blog Content</h3>
 					</div>
 				</div>
 			</div>
 		</header>
 
+		<div class="wrapper">
+			<div class="container">
+				<div class="news-content">
+					<div class="news-content-inner">
+						<div class="News-socials-wrapper">
+							<div>
+								<div class="cat">
+									<h5 class="title-caption">Tagged with: </h5>
+									<a href="#"><span>Blog</span></a>
+									<a href="#"><span>Creative</span></a>
+									<a href="#"><span>Design</span></a>
+								</div>
+							</div>
+
+							<div>
+								<div class="post-share">
+									<h5 class="title-caption">Share with:</h5>
+									<ul>
+										<li><a href="#" target="_blank">Facebook</a></li>
+										<li><a href="#" target="_blank">Twitter</a></li>
+										<li><a href="#" target="_blank">Linkedin</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+
+						<div class="post-content">
+							<p>
+								<a href="#">As a leading UX design agency,</a> we’re often asked
+								by prospective clients and fellow designers similar questions: Disrupt is an
+								experience
+								to showcase
+								the
+								agency's approach to growing brands by building tech. Featured in an FWA Insights
+								article, our team
+								elaborates on the motivation behind the creation of this site.
+							</p>
+
+							<blockquote class="wp-block-quote">
+								<p><strong><em>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
+											auctor.
+											Morbi leo risus,
+											porta ac consectetur ac, vestibulum at eros. Curabitulandit Sed posuere
+											consectetur
+											est at
+											lobortis. Donec id elit non mi porta gravida at eget
+											metus.</em></strong></p>
+							</blockquote>
+
+							<p>
+								<a href="#">As a leading UX design agency,</a> we’re often asked
+								by prospective clients and fellow designers similar questions: Disrupt is an
+								experience
+								to showcase
+								the
+								agency's approach to growing brands by building tech. Featured in an FWA Insights
+								article, our team
+								elaborates on the motivation behind the creation of this site.
+							</p>
+
+							<img class="w-100" src="assets/img/blog/1.jpg" alt="">
+
+							<p>
+								<a href="#">As a leading UX design agency,</a> we’re often asked
+								by prospective clients and fellow designers similar questions: Disrupt is an
+								experience
+								to showcase
+								the
+								agency's approach to growing brands by building tech. Featured in an FWA Insights
+								article, our team
+								elaborates on the motivation behind the creation of this site.
+							</p>
+
+						</div>
+					</div>
+
+					<div class="comments-post m-section">
+						<div class="comments-area section-margin">
+							<div class="comments-title">
+								<h4 class="subtitle">Comments</h4>
+							</div>
+
+							<ol class="comment-list">
+								<li class="comment">
+									<div class="comment-body">
+										<div class="comment-author">
+											<img alt="" src="assets/img/avatar/2.jpg">
+										</div>
+										<div class="comment-text">
+											<div class="comment-date">January 23, 2019</div>
+											<div class="comment-info">
+												<h6 class="comment-name">
+													Diana Torres </h6>
+											</div>
+											<div class="text-holder">
+												<p>Rassa id neque aliquam vestibulum morbi blandit in fermentum et
+													sollicitudin ac orci
+													phasellus egestas tellus velit ut tortor pretium viverra
+													suspendisse
+													potenti nullam ac
+													tortor
+													eu turpis egestas pretium.</p>
+											</div>
+											<a class="comment-reply-link" href="#"><i class="fas fa-reply"></i>
+												reply</a>
+										</div>
+									</div>
+
+									<ul class="children">
+										<li class="comment">
+											<div class="comment-body">
+												<div class="comment-author">
+													<img alt="" src="assets/img/avatar/1.jpg">
+												</div>
+												<div class="comment-text">
+													<div class="comment-date">January 23, 2019</div>
+													<div class="comment-info">
+														<h6 class="comment-name">
+															Diana Torres </h6>
+													</div>
+													<div class="text-holder">
+														<p>Rassa id neque aliquam vestibulum morbi blandit in
+															fermentum
+															et sollicitudin ac orci
+															phasellus egestas tellus velit ut tortor pretium viverra
+															suspendisse potenti nullam ac
+															tortor
+															eu turpis egestas pretium.</p>
+													</div>
+													<a class="comment-reply-link" href="#"><i
+																class="fas fa-reply"></i>
+														reply</a>
+												</div>
+											</div>
+										</li>
+									</ul>
+								</li>
+
+								<li class="comment">
+									<div class="comment-body">
+										<div class="comment-author">
+											<img alt="" src="assets/img/avatar/2.jpg">
+										</div>
+										<div class="comment-text">
+											<div class="comment-date">January 23, 2019</div>
+											<div class="comment-info">
+												<h6 class="comment-name">
+													Diana Torres </h6>
+											</div>
+											<div class="text-holder">
+												<p>Rassa id neque aliquam vestibulum morbi blandit in fermentum et
+													sollicitudin ac orci
+													phasellus egestas tellus velit ut tortor pretium viverra
+													suspendisse
+													potenti nullam ac
+													tortor
+													eu turpis egestas pretium.</p>
+											</div>
+											<a class="comment-reply-link" href="#"><i class="fas fa-reply"></i>
+												reply</a>
+										</div>
+									</div>
+								</li>
+
+							</ol>
+						</div>
+
+						<div class="comments-form">
+							<div class="comments-title">
+								<h4 class="subtitle">Comments</h4 class="subtitle">
+							</div>
+
+							<form>
+								<div class="entry">
+									<label>What's your name?</label>
+									<input id="form_name" type="text" name="name" placeholder="Type your name"
+										   required="required" data-error="name is required.">
+								</div>
+
+								<div class="entry">
+									<label>What's your email address?</label>
+									<input id="form_email" type="email" name="email"
+										   placeholder="Type your Email Address" required="required"
+										   data-error="Valid email is required.">
+								</div>
+
+								<div class="entry">
+									<label>comment?</label>
+									<textarea id="" class="form-control" name="message"
+											  placeholder="Tell us about you and the world" required="required"
+											  data-error="Your comment"></textarea>
+								</div>
+
+								<div class="submit-div image-zoom" data-dsn="parallax">
+									<input name="submit" type="submit" class="custom-btn" value="submit"
+										   data-parallex>
+								</div>
+							</form>
+						</div>
+
+
+					</div>
+
+
+				</div>
+
+
+			</div>
+
+			<section class="contact-up next-post-up section-margin section-padding">
+				<div class="container">
+					<div class="c-wapp">
+						<a href="contact.html" class="effect-ajax">
+                                <span class="hiring">
+                                    A lovely trip to London
+                                </span>
+							<span class="career">
+                                    Next Post
+                                </span>
+						</a>
+					</div>
+				</div>
+			</section>
+
+			<footer>
+				<div class="info">
+					<div class="contact-footer">
+						<a href="tel:010" class="phone">012.345.6789</a>
+						<a href="#" class="email">info@dsngrid</a>
+					</div>
+					<div class="copyright-social">
+						<p>© 2019 Design Grid</p>
+						<ul>
+							<li><a href="#" target="_blank">Instagram</a></li>
+							<li><a href="#" target="_blank">Facebook</a></li>
+							<li><a href="#" target="_blank">Linkedin</a></li>
+						</ul>
+					</div>
+				</div>
+			</footer>
+		</div>
 	</div>
 </main>
 <?php include(APPPATH.'views\layout\js.php')?>
