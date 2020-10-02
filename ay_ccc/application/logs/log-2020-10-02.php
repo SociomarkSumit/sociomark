ee9cf1b6d862806b0948d0e0c199aa3121d99f21
SociomarkSumit <71916852+SociomarkSumit@users.noreply.github.com>
2020-10-02 14:26:47 +0530

    thumbnail image feature added

---
+++ ay_ccc/application/logs/log-2020-10-02.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-10-02 10:05:04 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sociomark\ay_ccc\application\views\media\index.php 39
+ERROR - 2020-10-02 10:22:11 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sociomark\ay_ccc\application\views\media\index.php 39
+ERROR - 2020-10-02 10:47:11 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sociomark\ay_ccc\application\views\media\index.php 39
+ERROR - 2020-10-02 10:47:47 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sociomark\ay_ccc\application\views\media\index.php 39
+ERROR - 2020-10-02 10:48:15 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sociomark\ay_ccc\application\views\media\index.php 39
+ERROR - 2020-10-02 10:48:54 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sociomark\ay_ccc\application\views\media\index.php 39
