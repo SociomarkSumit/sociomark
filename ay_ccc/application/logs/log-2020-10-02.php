34747ace041a0673f0f2aed5e40ff779cd1ad76d
SociomarkSumit <71916852+SociomarkSumit@users.noreply.github.com>
2020-10-02 14:28:22 +0530

    Thumbnail controller name changes

--- ay_ccc/application/logs/log-2020-10-02.php
+++ ay_ccc/application/logs/log-2020-10-02.php
@@ -6,3 +6,8 @@ ERROR - 2020-10-02 10:47:11 --> Severity: Warning --> Invalid argument supplied 
 ERROR - 2020-10-02 10:47:47 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sociomark\ay_ccc\application\views\media\index.php 39
 ERROR - 2020-10-02 10:48:15 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sociomark\ay_ccc\application\views\media\index.php 39
 ERROR - 2020-10-02 10:48:54 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sociomark\ay_ccc\application\views\media\index.php 39
+ERROR - 2020-10-02 10:57:04 --> 404 Page Not Found: Thumbnail/index
+ERROR - 2020-10-02 10:57:06 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sociomark\ay_ccc\application\views\media\index.php 39
+ERROR - 2020-10-02 10:57:08 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sociomark\ay_ccc\application\views\media\index.php 39
+ERROR - 2020-10-02 10:57:10 --> 404 Page Not Found: Thumbnail/index
+ERROR - 2020-10-02 10:57:11 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sociomark\ay_ccc\application\views\media\index.php 39
