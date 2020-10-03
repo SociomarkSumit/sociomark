60596af800c5589e56af87526230e0a69a7c8885
sociomark <68726724+sociomark@users.noreply.github.com>
2020-10-03 11:31:04 +0530

    commit 1

---
+++ ay_ccc/application/logs/log-2020-10-02.php
@@ -0,0 +1,29 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-10-02 09:51:58 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'sociomark' C:\xampp\htdocs\sociomark\ay_ccc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-10-02 09:51:58 --> Unable to connect to the database
+ERROR - 2020-10-02 09:51:58 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'sociomark' C:\xampp\htdocs\sociomark\ay_ccc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-10-02 09:51:58 --> Unable to connect to the database
+ERROR - 2020-10-02 09:53:23 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'sociomark' C:\xampp\htdocs\sociomark\ay_ccc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-10-02 09:53:23 --> Unable to connect to the database
+ERROR - 2020-10-02 09:53:53 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'sociomark' C:\xampp\htdocs\sociomark\ay_ccc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-10-02 09:53:53 --> Unable to connect to the database
+ERROR - 2020-10-02 09:53:53 --> Severity: error --> Exception: Call to a member function real_escape_string() on bool C:\xampp\htdocs\sociomark\ay_ccc\system\database\drivers\mysqli\mysqli_driver.php 393
+ERROR - 2020-10-02 09:53:57 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'sociomark' C:\xampp\htdocs\sociomark\ay_ccc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-10-02 09:53:57 --> Unable to connect to the database
+ERROR - 2020-10-02 09:53:57 --> Severity: error --> Exception: Call to a member function real_escape_string() on bool C:\xampp\htdocs\sociomark\ay_ccc\system\database\drivers\mysqli\mysqli_driver.php 393
+ERROR - 2020-10-02 09:53:58 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'sociomark' C:\xampp\htdocs\sociomark\ay_ccc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-10-02 09:53:58 --> Unable to connect to the database
+ERROR - 2020-10-02 09:53:58 --> Severity: error --> Exception: Call to a member function real_escape_string() on bool C:\xampp\htdocs\sociomark\ay_ccc\system\database\drivers\mysqli\mysqli_driver.php 393
+ERROR - 2020-10-02 09:53:59 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'sociomark' C:\xampp\htdocs\sociomark\ay_ccc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-10-02 09:53:59 --> Unable to connect to the database
+ERROR - 2020-10-02 09:53:59 --> Severity: error --> Exception: Call to a member function real_escape_string() on bool C:\xampp\htdocs\sociomark\ay_ccc\system\database\drivers\mysqli\mysqli_driver.php 393
+ERROR - 2020-10-02 09:54:00 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'sociomark' C:\xampp\htdocs\sociomark\ay_ccc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-10-02 09:54:00 --> Unable to connect to the database
+ERROR - 2020-10-02 09:54:00 --> Severity: error --> Exception: Call to a member function real_escape_string() on bool C:\xampp\htdocs\sociomark\ay_ccc\system\database\drivers\mysqli\mysqli_driver.php 393
+ERROR - 2020-10-02 09:54:00 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'sociomark' C:\xampp\htdocs\sociomark\ay_ccc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-10-02 09:54:00 --> Unable to connect to the database
+ERROR - 2020-10-02 09:54:00 --> Severity: error --> Exception: Call to a member function real_escape_string() on bool C:\xampp\htdocs\sociomark\ay_ccc\system\database\drivers\mysqli\mysqli_driver.php 393
+ERROR - 2020-10-02 09:54:09 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'sociomark' C:\xampp\htdocs\sociomark\ay_ccc\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-10-02 09:54:09 --> Unable to connect to the database
+ERROR - 2020-10-02 09:54:09 --> Severity: error --> Exception: Call to a member function real_escape_string() on bool C:\xampp\htdocs\sociomark\ay_ccc\system\database\drivers\mysqli\mysqli_driver.php 393
